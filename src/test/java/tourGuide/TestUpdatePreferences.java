30ad3c9c3625d8da04e4847f7f9ca5c44ffbb921
Olivier <amoliv_825@hotmail.com>
2022-07-26 10:14:53 +0200

    correction test updatPreferences()

--- src/test/java/tourGuide/TestUpdatePreferences.java
+++ src/test/java/tourGuide/TestUpdatePreferences.java
@@ -1,11 +1,8 @@
 package tourGuide;
 
-import gpsUtil.GpsUtil;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import rewardCentral.RewardCentral;
 import tourGuide.helper.InternalTestHelper;
-import tourGuide.service.RewardsService;
 import tourGuide.service.TourGuideService;
 import tourGuide.user.UserPreferences;
 
@@ -14,23 +11,21 @@ import static org.junit.Assert.*;
 public class TestUpdatePreferences {
 
     @Autowired TourGuideController tourGuideController;
+    @Autowired TourGuideService tourGuideService;
 
     @Test
     public void updatePreferences() {
 
         //GIVEN
-        GpsUtil gpsUtil = new GpsUtil();
-        RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
-        TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
         InternalTestHelper.setInternalUserNumber(0);
         UserPreferences newPreferences = new UserPreferences();
         newPreferences.setNumberOfChildren(4);
         String userName = "internalUser1";
 
         //WHEN
-        tourGuideController.updateUserPreferences(newPreferences,userName);
+        tourGuideService.getUser(userName).setUserPreferences(newPreferences);
 
-        assertEquals(4,newPreferences.getNumberOfChildren());
+        assertEquals(4, tourGuideService.getUser(userName).getUserPreferences().getNumberOfChildren());;
 
 
 
