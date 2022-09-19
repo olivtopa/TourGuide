e7e63783a4657fb0abdbeca3c485e1f1e6275a9a
Olivier <amoliv_825@hotmail.com>
2022-09-19 19:28:23 +0200

    maj gradle.build pour Retrofit

--- src/main/java/tourGuide/service/GpsUtilService.java
+++ src/main/java/tourGuide/service/GpsUtilService.java
@@ -4,9 +4,7 @@ import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
 import org.springframework.stereotype.Service;
-
-//import com.squareup.retrofit2;
-
+import retrofit2.Retrofit;
 
 import java.util.List;
 import java.util.UUID;
@@ -14,11 +12,9 @@ import java.util.UUID;
 @Service
 public class GpsUtilService {
 
-  /*  Retrofit retrofit = new Retrofit.builder()
-            .baseUrl("http://localhost:8081/getLocation/")
-            .build();
-
-   */
+   Retrofit retrofit = new Retrofit.Builder()
+           .baseUrl("http://localhost:8081/getLocation/")
+           .build();
 
     public VisitedLocation getUserLocation(UUID userId) {
         return new GpsUtil().getUserLocation(userId);
