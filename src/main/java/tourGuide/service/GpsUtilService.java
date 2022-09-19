eab288be63c73c1a68cccef89a11772e85ed0b4c
Olivier <amoliv_825@hotmail.com>
2022-09-19 17:26:18 +0200

    mise en place de Retrofit pour les appels a TourGuideGPS

--- src/main/java/tourGuide/service/GpsUtilService.java
+++ src/main/java/tourGuide/service/GpsUtilService.java
@@ -3,12 +3,22 @@ package tourGuide.service;
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
+import org.springframework.stereotype.Service;
+
+//import com.squareup.retrofit2;
 
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class GpsUtilService {
+
+  /*  Retrofit retrofit = new Retrofit.builder()
+            .baseUrl("http://localhost:8081/getLocation/")
+            .build();
+
+   */
 
     public VisitedLocation getUserLocation(UUID userId) {
         return new GpsUtil().getUserLocation(userId);
