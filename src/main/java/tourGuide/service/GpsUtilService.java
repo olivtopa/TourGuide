a8460793b713b8dc620b5fb559efdac35b9d81e0
Olivier <amoliv_825@hotmail.com>
2022-09-21 18:21:04 +0200

    creations endpoint userLocation et Attraction , Retrofit

--- src/main/java/tourGuide/service/GpsUtilService.java
+++ src/main/java/tourGuide/service/GpsUtilService.java
@@ -1,26 +1,45 @@
 package tourGuide.service;
 
 import gpsUtil.GpsUtil;
-import gpsUtil.location.Attraction;
-import gpsUtil.location.VisitedLocation;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import tourGuide.TourGuideController;
+import tourGuide.newGpsUtil.Attraction;
+import tourGuide.newGpsUtil.Location;
+import tourGuide.newGpsUtil.VisitedLocation;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class GpsUtilService {
-
-   Retrofit retrofit = new Retrofit.Builder()
-           .baseUrl("http://localhost:8081/getLocation/")
-           .build();
-
-    public VisitedLocation getUserLocation(UUID userId) {
-        return new GpsUtil().getUserLocation(userId);
-    }
-
-    public List<Attraction> getAttractions(){
-        return new GpsUtil().getAttractions();
-    }
+    String userName;
+    public interface GetUserLocation {
+        @GET("/getLocation {userName}")
+        Call<Location> userLocation(@Path("userName") String userName);
 }
+
+    public interface GetAttraction {
+        @GET("/getAttractions")
+        Call<List<Attraction>> getAttractions();
+}
+   Retrofit tourGuideGPS = new Retrofit.Builder()
+           .baseUrl("http://localhost:8081")
+           .build();
+//Retrofit for UserLocation
+    GetUserLocation retrofitUserLocation = tourGuideGPS.create(GetUserLocation.class);
+    Call<Location> callUserLocationSync = retrofitUserLocation.userLocation(userName);
+//Retrofit for Attractions
+    GetAttraction retrofitAttractions = tourGuideGPS.create(GetAttraction.class);
+    Call<List<Attraction>> callAttractionsSync = retrofitAttractions.getAttractions();
+
+
+
+}
+
