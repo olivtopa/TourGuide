294863ebe1942496385b441c8182d9f856c1c91c
Olivier <amoliv_825@hotmail.com>
2022-09-22 12:20:44 +0200

    Correction : Ajouts des methodes d appel Https et de la recup des resultats

--- src/main/java/tourGuide/service/GpsUtilService.java
+++ src/main/java/tourGuide/service/GpsUtilService.java
@@ -7,39 +7,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 import tourGuide.TourGuideController;
 import tourGuide.newGpsUtil.Attraction;
 import tourGuide.newGpsUtil.Location;
 import tourGuide.newGpsUtil.VisitedLocation;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class GpsUtilService {
-    String userName;
-    public interface GetUserLocation {
-        @GET("/getLocation {userName}")
-        Call<Location> userLocation(@Path("userName") String userName);
-}
 
-    public interface GetAttraction {
+    public interface GetUserLocation {
+        @GET("/getLocation")
+        Call<VisitedLocation> userLocation(@Query("userId)") UUID userId);
+    }
+
+    public interface GetAttractions {
         @GET("/getAttractions")
         Call<List<Attraction>> getAttractions();
-}
+    }
+
    Retrofit tourGuideGPS = new Retrofit.Builder()
            .baseUrl("http://localhost:8081")
+           .addConverterFactory(JacksonConverterFactory.create())
            .build();
-//Retrofit for UserLocation
-    GetUserLocation retrofitUserLocation = tourGuideGPS.create(GetUserLocation.class);
-    Call<Location> callUserLocationSync = retrofitUserLocation.userLocation(userName);
-//Retrofit for Attractions
-    GetAttraction retrofitAttractions = tourGuideGPS.create(GetAttraction.class);
-    Call<List<Attraction>> callAttractionsSync = retrofitAttractions.getAttractions();
 
 
+    public VisitedLocation getUserLocation(UUID userId) throws IOException {
+        GetUserLocation retrofitUserLocation = tourGuideGPS.create(GetUserLocation.class);
+        Call<VisitedLocation> callUserLocationSync = retrofitUserLocation.userLocation(userId);
+        return callUserLocationSync.execute().body();
+
+    }
+
+    public List<Attraction> getAttractions() throws IOException {
+        GetAttractions retrofitAttractions = tourGuideGPS.create(GetAttractions.class);
+        Call<List<Attraction>> callAttractionsSync = retrofitAttractions.getAttractions();
+        return callAttractionsSync.execute().body();
+    }
 
 }
 
