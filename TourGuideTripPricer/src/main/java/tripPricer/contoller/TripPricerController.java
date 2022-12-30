5141337f0b0f092b4df32419659315cc6eaed177
Olivier <amoliv_825@hotmail.com>
2022-12-30 16:28:42 +0100

    implementation du controller TripPricer

--- TourGuideTripPricer/src/main/java/tripPricer/contoller/TripPricerController.java
+++ TourGuideTripPricer/src/main/java/tripPricer/contoller/TripPricerController.java
@@ -1,8 +1,30 @@
 package tripPricer.contoller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import tripPricer.Provider;
+import tripPricer.service.TripPricerService;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class TripPricerController {
+
+    @Autowired
+    TripPricerService tripPricerService;
+
+    @RequestMapping("/getPrice")
+    public List<Provider> getPrice (@RequestParam String apiKey, @RequestParam UUID attractionId,
+                                    @RequestParam int adults, @RequestParam int children, @RequestParam int nightsStay, @RequestParam int rewardsPoints){
+        return tripPricerService.getPrice(apiKey, attractionId, adults, children, nightsStay, rewardsPoints);
+    }
+
+    @RequestMapping("/getProviderName")
+    public String getProviderName (@RequestParam String apiKey, @RequestParam int adults){
+        return tripPricerService.getProviderName(apiKey, adults);
+    }
 
 }
